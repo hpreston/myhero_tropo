e0471208f19005080825f60ebe7025e68ba58508
Hank Preston <hank.preston@gmail.com>
2016-05-04 00:23:01 -0600

    Added support to auto-build the tropo app

--- myhero_tropo/myhero_tropo.py
+++ myhero_tropo/myhero_tropo.py
@@ -21,22 +21,29 @@
 
 __author__ = 'hapresto'
 
+# Todo - Convert back to flask "json.dumps(page.json)"
+# Todo - Add in Auth Code for API calls
 
 # from flask import Flask, request, Response
 import requests, json, re
+from requests.auth import HTTPBasicAuth
 from itty import *
-
 from tropo import Tropo, Session
 
 # app = Flask(__name__)
 
+# Tropo API Details
+tropo_host = "https://api.tropo.com/v1"
+tropo_headers = {}
+tropo_headers["Content-type"] = "application/json"
 
+# MyHero APP Details
 app_headers = {}
 app_headers["Content-type"] = "application/json"
 
 
 # @app.route('/', methods=["POST"])
-@post('/index.json')
+@post('/')
 def index(request):
     t = Tropo()
 
@@ -77,6 +84,14 @@ def index(request):
     # t.say(["Really, it's that easy." + message])
     t.say(reply)
     return t.RenderJson()
+
+@get('/application')
+def display_tropo_application(request):
+    return json.dumps(demoapp)
+
+@get('/application/number')
+def display_tropo_application_number(request):
+    return json.dumps(demoappnumber)
 
 # Utilities to interact with the MyHero-App Server
 def get_results():
@@ -122,6 +137,86 @@ def process_incoming_message(message):
         msg = "I didn't understand your vote, please type the name of your chosen hero exactly as listed on the ballot.  "
     return msg
 
+# Tropo Utilities
+def get_applications():
+    tropo_u = tropo_host + "/applications"
+    page = requests.get(tropo_u, headers = tropo_headers, auth=HTTPBasicAuth(tropo_user, tropo_pass))
+    applications = page.json()
+    return applications
+
+def get_application_addresses(application):
+    tropo_u = tropo_host + "/applications/%s/addresses" % (application["id"])
+    page = requests.get(tropo_u, headers = tropo_headers, auth=HTTPBasicAuth(tropo_user, tropo_pass))
+    addresses = page.json()
+    return addresses
+
+def create_application(appname, appurl):
+    data = {
+    "name":appname,
+    "voiceUrl":appurl,
+    "messagingUrl":appurl,
+    "platform":"webapi",
+    "partition":"staging"
+    }
+
+    tropo_u = tropo_host + "/applications"
+    page = requests.post(tropo_u, headers = tropo_headers, auth=HTTPBasicAuth(tropo_user, tropo_pass), json=data)
+    appurl = page.json()["href"]
+
+    page = requests.get(appurl, headers = tropo_headers, auth=HTTPBasicAuth(tropo_user, tropo_pass))
+    app = page.json()
+    return app
+
+def set_application_url(application, appurl):
+    data = {
+    "name":application["name"],
+    "voiceUrl":appurl,
+    "messagingUrl":appurl,
+    "platform":"webapi",
+    "partition":"staging"
+    }
+
+    tropo_u = tropo_host + "/applications/%s" % (application["id"])
+    page = requests.put(tropo_u, headers = tropo_headers, auth=HTTPBasicAuth(tropo_user, tropo_pass), json=data)
+    appurl = page.json()["href"]
+
+    page = requests.get(appurl, headers = tropo_headers, auth=HTTPBasicAuth(tropo_user, tropo_pass))
+    app = page.json()
+    return app
+
+def add_number(application, prefix):
+    data = {
+    "type":"number",
+    "prefix":prefix
+    }
+
+    tropo_u = tropo_host + "/applications/%s/addresses" % (application["id"])
+    page = requests.post(tropo_u, headers = tropo_headers, auth=HTTPBasicAuth(tropo_user, tropo_pass), json=data)
+    addressurl = page.json()["href"]
+
+    page = requests.get(addressurl, headers = tropo_headers, auth=HTTPBasicAuth(tropo_user, tropo_pass))
+    address = page.json()
+    return address
+
+# Standard Utility
+def valid_request_check(request):
+    try:
+        if request.headers["key"] == secret_key:
+            return (True, "")
+        else:
+            error = {"Error": "Invalid Key Provided."}
+            print error
+            status = 401
+            resp = Response(json.dumps(error), content_type='application/json', status=status)
+            return (False, resp)
+    except KeyError:
+        error = {"Error": "Method requires authorization key."}
+        print error
+        status = 400
+        resp = Response(json.dumps(error), content_type='application/json', status=status)
+        return (False, resp)
+
+
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
@@ -138,6 +233,18 @@ if __name__ == '__main__':
     )
     parser.add_argument(
         "-s", "--secret", help="Key Expected in API Calls", required=False
+    )
+    parser.add_argument(
+        "-t", "--tropouser", help="Tropo Username", required=False
+    )
+    parser.add_argument(
+        "-p", "--tropopass", help="Tropo Password", required=False
+    )
+    parser.add_argument(
+        "--tropoprefix", help="Tropo Number Prefix", required=False
+    )
+    parser.add_argument(
+        "--tropourl", help="Local Address for Tropo App URL", required=False
     )
 
     args = parser.parse_args()
@@ -166,17 +273,94 @@ if __name__ == '__main__':
     # print "App Server Key: " + app_key
     sys.stderr.write("App Server Key: " + app_key + "\n")
 
-    # secret_key = args.secret
-    # if (secret_key == None):
-    #     secret_key = os.getenv("myhero_spark_bot_secret")
-    #     if (secret_key == None):
-    #         get_secret_key = raw_input("What is the Authorization Key to Require? ")
-    #         secret_key = get_secret_key
-    # sys.stderr.write("Secret Key: " + secret_key + "\n")
+    secret_key = args.secret
+    if (secret_key == None):
+        secret_key = os.getenv("myhero_tropo_secret")
+        if (secret_key == None):
+            get_secret_key = raw_input("What is the Authorization Key to Require? ")
+            secret_key = get_secret_key
+    sys.stderr.write("Secret Key: " + secret_key + "\n")
+
+    tropo_user = args.tropouser
+    if (tropo_user == None):
+        tropo_user = os.getenv("myhero_tropo_user")
+        if (tropo_user == None):
+            get_tropo_user = raw_input("What is the Tropo Username? ")
+            tropo_user = get_tropo_user
+    sys.stderr.write("Tropo User: " + tropo_user + "\n")
+
+    tropo_prefix = args.tropoprefix
+    if (tropo_prefix == None):
+        tropo_prefix = os.getenv("myhero_tropo_prefix")
+        if (tropo_prefix == None):
+            get_tropo_prefix = raw_input("What is the Tropo Prefix? ")
+            tropo_prefix = get_tropo_prefix
+    sys.stderr.write("Tropo Prefix: " + tropo_prefix + "\n")
+
+    tropo_pass = args.tropopass
+    if (tropo_pass == None):
+        tropo_pass = os.getenv("myhero_tropo_pass")
+        if (tropo_pass == None):
+            get_tropo_pass = raw_input("What is the Tropo Password? ")
+            tropo_pass = get_tropo_pass
+    sys.stderr.write("Tropo Pass: " + tropo_pass + "\n")
+
+    tropo_url = args.tropourl
+    if (tropo_url == None):
+        tropo_url = os.getenv("myhero_tropo_url")
+        if (tropo_url == None):
+            get_tropo_url = raw_input("What is the Local Tropo App URL? ")
+            tropo_url = get_tropo_url
+    sys.stderr.write("Tropo URL: " + tropo_url + "\n")
 
 
     # Set Authorization Details for external requests
     app_headers["key"] = app_key
+
+    # Find if Tropo Application "myherodemo" already exists
+    # If not, create it
+    # If exists, verify has correct url and a number in the correct prefix
+    tropo_applications = get_applications()
+
+    demoappid = ""
+    demoapp = {}
+    demoappnumber = ""
+    demoappprefix = ""
+
+    for app in tropo_applications:
+        if app["name"] == "myherodemo":
+            # pprint("Found Demo App")
+            demoappid = app["id"]
+            demoapp = app
+            if demoapp["messagingUrl"] != tropo_url:
+                pprint("Updated App URLs")
+                demoapp = set_application_url(demoapp, tropo_url)
+
+    if demoappid == "":
+        pprint("Creating Tropo App")
+        demoapp = create_application("myherodemo", tropo_url)
+        demoappid = demoapp["id"]
+        # pprint(demoapp)
+
+    pprint("Tropo App: %s - %s" % (demoappid, demoapp["name"]))
+    # pprint(demoapp)
+
+    addresses = get_application_addresses(demoapp)
+    for address in addresses:
+        if address["type"] == "number" and address["prefix"] == tropo_prefix:
+            # pprint("Found Address")
+            demoappnumber = address["number"]
+            demoappprefix = address["prefix"]
+
+    if demoappnumber == "":
+        pprint ("Creating Tropo Number")
+        address = add_number(demoapp, tropo_prefix)
+        demoappnumber = address["number"]
+        demoappprefix = address["prefix"]
+
+    pprint("Tropo Number: " + demoappnumber)
+
+
 
     # app.run(debug=True, host='0.0.0.0', port=int("5000"))
     run_itty(server='wsgiref', host='0.0.0.0', port=5000)
